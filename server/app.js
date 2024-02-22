6e8e59284690d70e92264e10853841c4bb6cf161
Threx171 <alejandro_1173@hotmail.com>
2024-02-22 15:40:30 +0100

    fix

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ app.post('/data', upload.single('file'), async (req, res) => {
     // Process form data and attached file
     console.log(req.body);
     const textPost = req.body;
-    console.log(textPost);
+    console.log(textPost.data);
     const uploadedFile = req.file;
     let objPost = {}
   
