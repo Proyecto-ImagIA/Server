914cba0211853d9ca92174df308435410df7d0e2
Threx171 <alejandro_1173@hotmail.com>
2024-03-05 16:41:54 +0100

    logs

--- server/app.js
+++ server/app.js
@@ -254,6 +254,7 @@ app.post('/data', upload.single('file'), async (req, res) => {
                 res.status(500).send('Internal Server Error');
             })
             .finally(() => {
+              console.log('Petition processed');
               res.end();
             });
           }
